e47a0cfa661ea1965844403e724d01dab904375a
Aditya Prasad <ap379436@gmail.com>
2020-07-15 10:30:10 +0530

    With comments

--- chefandprices.cpp
+++ chefandprices.cpp
@@ -5,21 +5,21 @@ int main(){
 	int T=0,n=0,k=0;
 	int loss=0;
 	cin>>T;
-	while(T>0){
+	while(T>0){				// For T number of testcases;
 		loss=0;
-		cin>>n>>k;
+		cin>>n>>k;			//Take input for number of inputs and K:threshold price;
 		for(int i=0;i<n;i++){
 		int a=0;
 		cin>>a;
-		if(a>k){
-			loss+=(a-k);
+		if(a>k){			//Checking if price >K;
+			loss+=(a-k);	//If greater than K then add to loss;
 			}
 
 		}
-		cout<<loss<<endl;
+		cout<<loss<<endl; //print the total loss for the testcase;
 
 
 
-		T--;
+		T--;				//decrement iterator;
 	}
 }
